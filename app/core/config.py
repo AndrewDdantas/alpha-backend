106a592b1a58eab4ec01a36973b1b9f5fd209de1
AndrewDdantas <andrewdantas15@gmail.com>
2026-01-05 11:30:33 -0300

    feat: add initial application configuration with Pydantic settings for various services.

--- app/core/config.py
+++ app/core/config.py
@@ -24,6 +24,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://andrewddantas.github.io",  # GitHub Pages
+        "https://andrewddantas.github.io/alpha-frontend"
     ]
 
     # JWT
