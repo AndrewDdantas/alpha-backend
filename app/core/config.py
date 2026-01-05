e0c497df846f074f109edb1bbc15f808961def06
AndrewDdantas <andrewdantas15@gmail.com>
2026-01-05 10:56:39 -0300

    Add GitHub Pages to CORS origins

--- app/core/config.py
+++ app/core/config.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://andrewddantas.github.io",  # GitHub Pages
     ]
 
     # JWT
