409cdaab7f90ba009ce6219a673c9f5c2c4da830
Andrew Dantas <andrewdantas15@gmail.com>
2026-07-19 01:38:49 -0300

    api

--- app/core/config.py
+++ app/core/config.py
@@ -36,6 +36,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://andrewddantas.github.io",
+        "https://alpha.marvsystems.com.br",
     ]
 
     # JWT
